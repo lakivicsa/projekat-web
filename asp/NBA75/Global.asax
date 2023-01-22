218cc9173df5c9921839dab46d198809ff0075e6
Lazar Savic <vicsalaki@gmail.com>
2023-01-22 18:51:26 +0100

    :wq

---
+++ asp/NBA75/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NBA75.Global" Language="C#" %>
---
+++ asp/NBA75/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NBA75.Startup))]
+namespace NBA75
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
